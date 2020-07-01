73c366d52247bb0c1e967036a41f2636017e8e76
Nathan Kartchner <nathan.kartchner@gmail.com>
2020-06-30 18:40:50 -0700

    brushup and update deps

--- server.js
+++ server.js
@@ -1,154 +1,168 @@
-
 //////////////////////////////////////////////////////////////////// DATABASE STRUCTURE ////////////////////////////////////////////////////////////////////
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/MessageBoard', { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/MessageBoard", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
 
 mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on("error", console.error.bind(console, "connection error:"));
 
-db.once('open', function () { });
+db.once("open", function () {});
 //////////////////////////// SCHEMA DEFINITIONS ////////////////////////////
 
-const CommentSchema = new mongoose.Schema({
+const CommentSchema = new mongoose.Schema(
+  {
     content: {
-        type: String,
-        required: [true, "You cannot post an empty comment"]
+      type: String,
+      required: [true, "You cannot post an empty comment"],
     },
     name: {
-        type: String,
-        required: [true, "You cannot post a comment anonymously"]
-    }
-}, { timestamps: true });
+      type: String,
+      required: [true, "You cannot post a comment anonymously"],
+    },
+  },
+  { timestamps: true }
+);
 
-const MessageSchema = new mongoose.Schema({
-
+const MessageSchema = new mongoose.Schema(
+  {
     content: {
-        type: String,
-        required: [true, "You cannot send an empty message"]
+      type: String,
+      required: [true, "You cannot send an empty message"],
     },
     name: {
-        type: String,
-        required: [true, "You cannot post a message anonymously"]
+      type: String,
+      required: [true, "You cannot post a message anonymously"],
     },
-    comments: [CommentSchema]
-
-
-}, { timestamps: true });
+    comments: [CommentSchema],
+  },
+  { timestamps: true }
+);
 
 //////////////////////////// MODEL INSTANTIATIONS ////////////////////////////
 
-const Message = mongoose.model('Message', MessageSchema);
+const Message = mongoose.model("Message", MessageSchema);
 
-const Comment = mongoose.model('Comment', CommentSchema);
+const Comment = mongoose.model("Comment", CommentSchema);
 
 //////////////////////////////////////////////////////////////////// EXPRESS VARIABLES ////////////////////////////////////////////////////////////////////
-const express = require('express');
+const express = require("express");
 
-const flash = require('express-flash');
+const flash = require("express-flash");
 
 const app = express();
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const path = require('path');
+const path = require("path");
 
-const session = require('express-session');
+const session = require("express-session");
 
 //////////////////////////////////////////////////////////////////// APP INSTANTIATION ////////////////////////////////////////////////////////////////////
-app.use(session({
-    secret: 'keyboardkitteh',
+app.use(
+  session({
+    secret: "keyboardsmashin",
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 60000 }
-}));
+    cookie: { maxAge: 60000 },
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.use(flash());
 
-app.set('views', path.join(__dirname, './views'));
+app.set("views", path.join(__dirname, "./views"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 /////////////////////////////// GET ///////////////////////////////////// MESSAGE BOARD ////////////////////////////////////////////////////////////////////
 
-app.get('/', function (request, response) {
-    Message.find({}).sort('-createdAt').exec(function (err, data) {
-        response.render('index', { data });
+app.get("/", function (request, response) {
+  Message.find({})
+    .sort("-createdAt")
+    .exec(function (err, data) {
+      response.render("index", { data });
     });
 });
 /////////////////////////////// POST ///////////////////////////////////// CREATE MESSAGE ////////////////////////////////////////////////////////////////////
 
-app.post('/message', function (request, response) {
-    let message = new Message(request.body);
-    message.save(function (err) {
-        if (err) {
-            console.log("Something went wrong!:", err);
-            for (var key in err.errors) {
-                console.log("Added flash message");
-                request.flash('registration', err.errors[key].message);
-            }
-            response.redirect('/');
-        } else {
-            response.redirect('/');
-
-        }
-    });
+app.post("/message", function (request, response) {
+  let message = new Message(request.body);
+  message.save(function (err) {
+    if (err) {
+      console.log("Something went wrong!:", err);
+      for (var key in err.errors) {
+        console.log("Added flash message");
+        request.flash("registration", err.errors[key].message);
+      }
+      response.redirect("/");
+    } else {
+      response.redirect("/");
+    }
+  });
 });
 
 /////////////////////////////// POST ///////////////////////////////////// CREATE COMMENT ////////////////////////////////////////////////////////////////////
-app.post('/comment', function (request, response) {
-    Comment.create({ content: request.body.cmnt, name: request.body.name }, function (err, data) {
-        if (err) {
-            console.log("Something went wrong!:", err);
-            for (var key in err.errors) {
-                request.flash('registration', err.errors[key].message);
-            }
-            response.redirect('/');
-        } else {
-            Message.findOneAndUpdate({ _id: request.body.message }, { $push: { comments: data } }, function (err) {
-                if (err) {
-                    console.log("Something went wrong when pushing the comment into the message!:", err);
-                    for (var key in err.errors) {
-                        request.flash('registration', err.errors[key].message);
-                    }
-                    response.redirect('/');
-                } else {
-                    console.log(`All went well`);
-                    response.redirect('/');
-                }
-            });
+app.post("/comment", function (request, response) {
+  Comment.create(
+    { content: request.body.cmnt, name: request.body.name },
+    function (err, data) {
+      if (err) {
+        console.log("Something went wrong!:", err);
+        for (var key in err.errors) {
+          request.flash("registration", err.errors[key].message);
         }
-    });
+        response.redirect("/");
+      } else {
+        Message.findOneAndUpdate(
+          { _id: request.body.message },
+          { $push: { comments: data } },
+          function (err) {
+            if (err) {
+              console.log(
+                "Something went wrong when pushing the comment into the message!:",
+                err
+              );
+              for (var key in err.errors) {
+                request.flash("registration", err.errors[key].message);
+              }
+              response.redirect("/");
+            } else {
+              response.redirect("/");
+            }
+          }
+        );
+      }
+    }
+  );
 });
-
 
 /////////////////////////////// POST ///////////////////////////////////// DELETE MESSAGE ////////////////////////////////////////////////////////////////////
-app.post('/destroy/:id', function (request, response) {
-    console.log(request.params.id);
-    Message.remove({ _id: request.params.id }, function (err) {
-        if (err) {
-            console.log("Something went wrong!:", err);
-            for (var key in err.errors) {
-                request.flash('registration', err.errors[key].message);
-            }
-            response.redirect('/');
-        } else {
-            console.log('deleted');
-            response.redirect('/');
-        }
-    });
-
+app.post("/destroy/:id", function (request, response) {
+  console.log(request.params.id);
+  Message.remove({ _id: request.params.id }, function (err) {
+    if (err) {
+      console.log("Something went wrong!:", err);
+      for (var key in err.errors) {
+        request.flash("registration", err.errors[key].message);
+      }
+      response.redirect("/");
+    } else {
+      console.log("deleted");
+      response.redirect("/");
+    }
+  });
 });
 
-
-app.listen(5000, function () {
-    console.log("listening on port 5000");
+app.listen(5004, function () {
+  console.log("listening on port 5004");
 });
-
